c3f41fb65841b1fabca880c0b4b4b6e79d996ff8
Chamod pinimal <chamod_20221397@fot.ruh.ac.lk>
2024-11-02 19:01:11 +0530

    Update views.sql

--- views.sql
+++ views.sql
@@ -2,7 +2,7 @@ CREATE VIEW QuizMarks AS
 SELECT 
     student_id,
     course_code,
-    department_id,
+    dep_id,
     (GREATEST(quiz1, quiz2, quiz3) * 0.025 + 
      GREATEST(LEAST(quiz1, quiz2), LEAST(GREATEST(quiz1, quiz2), quiz3)) * 0.025) AS total_quiz_mark
 FROM 
