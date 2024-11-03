056834d931a89124b618916a3e8f38ee8aa58913
Chamod pinimal <chamod_20221397@fot.ruh.ac.lk>
2024-11-03 16:25:00 +0530

    Update views.sql

--- views.sql
+++ views.sql
@@ -41,3 +41,36 @@ FROM
     Marks  
 JOIN 
     Student ON Marks.student_id = Student.student_id;
+
+
+
+--Final marks view
+
+CREATE VIEW Final_Marks AS
+SELECT 
+    Marks.student_id,
+    Marks.course_code,
+    (
+        LEAST(CA_Marks.total_ca_marks, 100) * 0.4 +  -- Scale CA to a maximum of 100, then apply 40% weight
+        Marks.end_theory * 0.3 +                      -- 30% weight for end theory
+        Marks.end_practical * 0.3                     -- 30% weight for end practical
+    ) AS final_exam_marks,
+    CASE 
+        WHEN Student.status = 'suspended' THEN 'WH'
+        WHEN Marks.medical_submission_id IS NOT NULL THEN 'MC'
+        WHEN Exam_eligibility_view.Exam_Eligibility = 'Eligible' THEN 'Eligible'
+        ELSE 'Not Eligible'
+    END AS final_status
+FROM 
+    Marks
+JOIN 
+    CA_Marks ON Marks.student_id = CA_Marks.student_id 
+             AND Marks.course_code = CA_Marks.course_code
+JOIN 
+    Student ON Marks.student_id = Student.student_id
+JOIN 
+    Exam_eligibility_view ON Marks.student_id = Exam_eligibility_view.student_id 
+                         AND Marks.course_code = Exam_eligibility_view.course_code;
+
+
+
