c13114e8abda8dcccb243d36699806a0566d130b
Chamod pinimal <chamod_20221397@fot.ruh.ac.lk>
2024-11-05 06:49:32 +0530

    Update user.sql

--- user.sql
+++ user.sql
@@ -1,14 +1,15 @@
 CREATE USER 'admin'@'localhost' IDENTIFIED BY '12345';
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON group11.* TO 'admin'@'localhost' WITH GRANT OPTION;
 
 CREATE USER 'dean'@'localhost' IDENTIFIED BY '12345';
-GRANT ALL PRIVILEGES ON project11.* TO 'dean'@'localhost';
+GRANT ALL PRIVILEGES ON group11.* TO 'dean'@'localhost';
 
 CREATE USER 'lecturer'@'localhost' IDENTIFIED BY '12345';
-GRANT SELECT, INSERT, UPDATE, DELETE ON projeect11.* TO 'lecturer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON group11.* TO 'lecturer'@'localhost';
 
 CREATE USER 'tech_officer'@'localhost' IDENTIFIED BY '12345';
-GRANT SELECT, INSERT, UPDATE ON project11.Attendance TO 'tech_officer'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON group11.Attendance TO 'tech_officer'@'localhost';
 
 CREATE USER 'student'@'localhost' IDENTIFIED BY '12345';
-GRANT SELECT ON project11.Attendance, project11.Marks TO 'student'@'localhost';
+GRANT SELECT ON group11.Attendance TO 'student'@'localhost';
+GRANT SELECT ON group11.Marks TO 'student'@'localhost';
